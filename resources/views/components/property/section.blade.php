992af4a76f688340222537fff414995cc8775575
Fabi Faharudin Faiz <fabifahruddin06@gmail.com>
2026-09-08 03:44:14 +0700

    card property terbaru

--- resources/views/components/property/section.blade.php
+++ resources/views/components/property/section.blade.php
@@ -2,27 +2,28 @@
     'title' => 'Properti Terpopuler',
     'properties' => [],
     'viewAllUrl' => '#',
+    'background' => 'bg-white',
 ])
 
-<section class="w-full py-10">
+<section class="w-full py-10 {{ $background }}">
 
-    {{-- HEADER --}}
     <div class="max-w-[1200px] mx-auto px-5 lg:px-0">
 
+        {{-- HEADER --}}
         <div class="flex items-center justify-between mb-6">
 
             <h2 class="text-2xl md:text-3xl font-bold text-gray-900">
                 {{ $title }}
             </h2>
 
+            {{-- DESKTOP --}}
             <a
                 href="{{ $viewAllUrl }}"
-                class="hidden sm:inline-flex items-center justify-center
-                       px-6 py-2.5 rounded-full
+                class="hidden sm:flex items-center justify-center
+                       px-6 h-10 rounded-full
                        bg-gray-950 text-white
                        text-sm font-semibold
-                       hover:bg-orange-600
-                       transition"
+                       hover:bg-orange-500 transition"
             >
                 Lihat Semua
             </a>
@@ -42,11 +43,9 @@
             @empty
 
                 <div class="col-span-full py-16 text-center">
-
                     <p class="text-gray-500">
                         Belum ada properti.
                     </p>
-
                 </div>
 
             @endforelse
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -88,12 +88,65 @@
 
         ];
 
+        $latestProperties = [
+            [
+                'title' => 'Rumah Minimalis Baru di Jember',
+                'price' => 'Rp 750 Juta',
+                'location' => 'Sumbersari, Jember',
+                'image' => asset('images/property/rumah1.jpg'),
+                'status' => 'BARU',
+                'land_area' => 120,
+                'bedrooms' => 3,
+                'bathrooms' => 2,
+                'date' => 'Hari ini',
+            ],
+            [
+                'title' => 'Tanah Strategis Dekat Jalan Utama',
+                'price' => 'Rp 450 Juta',
+                'location' => 'Kaliwates, Jember',
+                'image' => asset('images/property/rumah2.jpg'),
+                'status' => 'BARU',
+                'land_area' => 200,
+                'bedrooms' => 0,
+                'bathrooms' => 0,
+                'date' => 'Hari ini',
+            ],
+            [
+                'title' => 'Rumah Nyaman Siap Huni',
+                'price' => 'Rp 900 Juta',
+                'location' => 'Patrang, Jember',
+                'image' => asset('images/property/rumah3.jpg'),
+                'status' => 'BARU',
+                'land_area' => 150,
+                'bedrooms' => 4,
+                'bathrooms' => 2,
+                'date' => 'Kemarin',
+            ],
+            [
+                'title' => 'Rumah Dekat Kampus',
+                'price' => 'Rp 680 Juta',
+                'location' => 'Tegal Boto, Jember',
+                'image' => asset('images/property/rumah4.jpg'),
+                'status' => 'BARU',
+                'land_area' => 100,
+                'bedrooms' => 3,
+                'bathrooms' => 2,
+                'date' => 'Kemarin',
+            ],
+        ];
     @endphp
 
     <x-property.section
     title="Properti Terpopuler"
     :properties="$popularProperties"
     view-all-url="#"
+    />
+
+    <x-property.section
+        title="Properti Terbaru"
+        :properties="$latestProperties"
+        view-all-url="#"
+        background="bg-[#FFF4EA]"
     />
 
     {{-- Footer --}}
